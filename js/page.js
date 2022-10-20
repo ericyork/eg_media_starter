6955f4123de1a6ff72087f1f30cfcab656bb1739
ericyork <eyork@clarkson.edu>
2022-10-19 22:45:06 -0400

    starter code
    implements starter code including page structure, basic styles and responsive grid, JS alert, and structure for mobile nav

--- js/page.js
+++ js/page.js
@@ -1,5 +1,3 @@
-// the code below tests whether js is loading: if it is, some text is blue
-window.addEventListener('load', () => {           // runs a nameless function when the window object is loaded
-  let testBit = document.getElementById('test');  // sets testBit equal to #test
-  testBit.classList.add('blue');                  // adds the class .blue to #test
-});
+function sampleFunction() {
+  alert("You clicked the button");
+}
